6ba8ce43a4afe8b910a514549d6574c4093b232a
Marc <marc.berli@windowslive.com>
2018-09-27 22:47:04 +0200

    SS04:   Made code stryleguide compliant

--- ss04-word-sort/src/main.c
+++ ss04-word-sort/src/main.c
@@ -20,61 +20,61 @@
 const char END_WORD[] = "ZZZ";
 
 // @brief compares two string based on their ASCII Value.
-int compare(const void * a, const void * b)
+static int compare(const void * a, const void * b)
 {
     return strcmp (*(const char **) a, *(const char **) b);
 }
 
 // @brief dedupes words and prints the sorted list
 // @attention This function depends on the fact that the list is already sorted!
-void printDedupedWordlist(char* words[MAX_WORD_COUNT], const int wordCount)
+static void print_deduped_word_list(char* words[MAX_WORD_COUNT], const int word_count)
 {
   char* deduped[MAX_WORD_COUNT];
-  int dedupedWordCount = 0;
-  for(int i = 0; i < wordCount; i++) {
+  int deduped_word_count = 0;
+  for(int i = 0; i < word_count; i++) {
     // if the last word is not equal this one, add it to the new array
     if (i == 0 || strcmp(words[i-1], words[i]) != 0) {
         // store word in deduped array
-        deduped[dedupedWordCount] = words[i];
-        dedupedWordCount += 1;
+        deduped[deduped_word_count] = words[i];
+        deduped_word_count += 1;
     }
   }
   (void) printf("--------------\n");
   (void) printf("----Sorted----\n");
   (void) printf("--------------\n");
-  for(int i = 0; i < dedupedWordCount; i++) {
+  for(int i = 0; i < deduped_word_count; i++) {
     (void) printf("%s\n", deduped[i]);
   }
   (void) printf("--------------\n");
 }
 
-int readWords(char* words[MAX_WORD_COUNT], int *wordCount)
+static int read_words(char* words[MAX_WORD_COUNT], int *word_count)
 {
     (void) printf("Geben Sie Woerter ein:\n");
     int i = 0;
     char word[MAX_WORD_SIZE];
-    int hasFailed = 0;
-    char* newWord;
+    int has_failed = 0;
+    char* new_word;
     do {
         (void) strcpy(word, END_WORD); // reset
         (void) scanf("%" S(MAX_WORD_SCAN_SIZE)"s", word);
         int len = strlen(word);
         if (len >= 0 && strcmp(END_WORD, word) != 0) {
-            newWord = (char *) malloc(len * sizeof(char));
-            if (newWord == NULL) {
+            new_word = (char *) malloc(len * sizeof(char));
+            if (new_word == NULL) {
                 (void) printf("not enough memory\n");
-                hasFailed = 1;
+                has_failed = 1;
             }
-            strcpy(newWord, word);
-            words[i] = newWord;
+            strcpy(new_word, word);
+            words[i] = new_word;
             i++;
         }
-    } while(hasFailed == 0 && strcmp(END_WORD, word) != 0);
-    if (hasFailed == 1) {
-        return 0;
+    } while(has_failed == 0 && strcmp(END_WORD, word) != 0);
+
+    if (has_failed == 0) {
+        *word_count = i;
     }
-    *wordCount = i;
-    return 1;
+    return has_failed;
 }
 
 /**
@@ -84,11 +84,11 @@ int readWords(char* words[MAX_WORD_COUNT], int *wordCount)
  */
 int main(void)
 {
-    char *wordList[MAX_WORD_COUNT];
-    int wordCount = 0;
-    if (readWords(wordList, &wordCount)) {
-        (void) qsort(wordList, wordCount, sizeof (const char *), compare);
-        printDedupedWordlist(wordList, wordCount);
+    char *word_list[MAX_WORD_COUNT];
+    int word_count = 0;
+    if (read_words(word_list, &word_count) == 0) {
+        (void) qsort(word_list, word_count, sizeof (const char *), compare);
+        print_deduped_word_list(word_list, word_count);
         return EXIT_SUCCESS;
     }
     return EXIT_FAILURE;
--- ss04-word-sort/src/main.h
+++ ss04-word-sort/src/main.h
@@ -20,8 +20,8 @@
 #define MAX_WORD_SIZE 20
 #define MAX_WORD_SCAN_SIZE 19
 
-int compare(const void *a, const void *b);
+static int compare(const void *a, const void *b);
 
-void printDedupedWordlist(char *words[MAX_WORD_COUNT], const int wordCount);
+static void printDedupedWordlist(char *words[MAX_WORD_COUNT], const int wordCount);
 
-int readWords(char *words[MAX_WORD_COUNT], int *wordCount);
+static int readWords(char *words[MAX_WORD_COUNT], int *wordCount);
