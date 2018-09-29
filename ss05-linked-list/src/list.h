6281a7db81124765357b9c8c9744669a1d97b714
Simon Stucki <simon_stucki@hotmail.com>
2018-09-29 14:00:27 +0200

    Add documentation

--- ss05-linked-list/src/list.h
+++ ss05-linked-list/src/list.h
@@ -9,15 +9,21 @@ struct LE {
     /// @brief person for the element
     Person content;
     /// @brief pointer to the next list element (or start)
-    ListElement* next;
+    ListElement *next;
 };
 
+/// @brief allocate a new list element and prompt the usert to enter user data
 ListElement *create_new_list_element(void);
 
+/// @brief insert a list element into a list - it will not be tested whether it
+/// already is included
 void insert_list_element(ListElement *start, ListElement *newElement);
 
+/// @brief search element in list and remove it - also frees memory
 void delete_list_element(ListElement *start, ListElement *element);
 
-void clear_list(ListElement* start);
+/// @brief remove all elements from list - also frees memory
+void clear_list(ListElement *start);
 
-void print_people(ListElement* start);
+/// @brief print all elements to stdout
+void print_people(ListElement *start);
